28d5e4a05a9c21d391f3890a14a186c6fdbdb60a
nikadzeladze-droid <nikadzeladze@gmail.com>
2025-10-18 11:09:25 +0400

    second commit

---
+++ level 02/hw/hw.py
@@ -0,0 +1,28 @@
+#string არის ნებისმიერი რამ რაც არის მოქცეული ბრჩხილებში py-ში
+#integer ნებისმიერი მთელი რიცხვი py-ში
+#float ნებისმიერი ათწილადი რიცხვი py-ში
+
+name = "laricoin"
+age = 2000 
+country = "georgia"
+city = "tbilisi"
+drink = "water"
+print(name , age , country , city , drink)
+
+print("laricoin")
+print("georgia")
+print("tbilisi")
+print("water")
+print("chad")
+
+print(52)
+print(12)
+print(69)
+print(67)
+print(2025)
+
+print(1.1)
+print(1.2)
+print(1.3)
+print(1.4)
+print(1.5)
