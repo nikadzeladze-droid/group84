3df342f46eee324ddb32f51e2af45902ba44689e
nikadzeladze-droid <nikadzeladze@gmail.com>
2025-10-18 11:11:09 +0400

    second commit

---
+++ level 03/hw/hw.py
@@ -0,0 +1,35 @@
+# კონკატინაცია არის 2 ცალი string-ის შეერთება
+
+num1 = 30
+num2 = 20
+num3 = 19
+print(num1 + num2 + num3)
+
+lengh = 10 
+width = 20
+print (lengh + width + lengh + width)
+
+print(20 + 20 + 29)
+print(29+20+20)
+print(50+19)
+
+print(69-20-90)
+print(2-4-10)
+print(10 - 20 - 30)
+
+print(2*2*3)
+print(2*5*10)
+print(2*10*100)
+
+print(100 / 10 / 10)
+print(1000 / 10 / 100)
+print(10000 / 100 / 100)
+
+name = "nika"
+age = 12 
+car = "BMW"
+food = "xinkali"
+
+print("hello my name is" , name , "i'm" , age , "years old my favorite car is" , car , "and my fav food is" , food)
+
+# აუ რა chad-ი ვარ 
