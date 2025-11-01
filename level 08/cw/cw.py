09f52daadf569ae3dcfcef3998083011817f77a4
nikadzeladze-droid <nikadzeladze@gmail.com>
2025-11-01 11:06:47 +0400

    second commit

---
+++ level 08/cw/cw.py
@@ -0,0 +1,24 @@
+#1
+name="მომხმარებლის სახელი : "
+
+input(name)
+
+#2
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+print("რიცხვების ჯამი არის:", num1 + num2)
+
+#3
+
+age = input("შეიყვანეთ თქვენი ასაკი: ")
+age = int(age)
+print("თქვენი ასაკის მონაცემთა ტიპია:", type(age))
+
+#4
+num = 10
+text = "გამარჯობა"
+price = 3.14
+
+print("num ცვლადის ტიპი არის:", type(num))
+print("text ცვლადის ტიპი არის:", type(text))
+print("price ცვლადის ტიპი არის:", type(price))
---
+++ level 08/hw/hw.py
@@ -0,0 +1,85 @@
+#2
+#input არის როდესაც ჩვენ შეგვყავს რაღაცა მაგალითად მიკროფონი მიკროფონით ჩვენ შეგვაქვს ხმა
+#output კი შემოტანილ მონაცემს აგვიქვამს მაგალითად მიკროფონით თუ შეიყვანეს ხმა ჩვენ გავიგეთ იგი ყურსასმენით ან დინამიკით 
+
+#3
+
+name = input("chad")
+print(type(name))
+
+
+
+
+
+
+
+
+
+
+
+
+#4
+#integer მთელი რიცხვების ტიპის ცვლადები
+
+age = 12
+year = 2012
+month = 12
+days_in_week = 7
+temperature = 15
+
+#str არის ტექსტური მონაცემი და ბრჭყალებში იწერება
+
+name = "nika"
+country = "Georgia"
+language = "Python"
+color = "blue"
+city = "Tbilisi"
+
+
+
+#float არის ათწილადი რიცხვები
+speed = 70.5
+age = 12.0
+school = 161.0
+time = 16.13
+month = 11 
+
+#5
+
+tuff = input(12)
+chad = input("nika")
+bruhh = input(67.67)
+
+print(type(tuff))
+print(type(chad))
+print(type(bruhh))
+
+#6
+
+log = input("create your login:")
+pas = input("create your password:")
+
+
+
+#7
+
+num1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+num3 = float(input("შეიყვანეთ მესამე რიცხვი: "))
+num4 = float(input("შეიყვანეთ მეოთხე რიცხვი: "))
+num5 = float(input("შეიყვანეთ მეხუთე რიცხვი: "))
+
+საშ = (num1 + num2 + num3 + num4 + num5) / 5
+
+print("რიცხვების საშუალო არითმეტიკულია:", საშ)
+
+
+#8
+
+სახელი=input("შეიყვანეთ თქვენი სახელი")
+გვარი=input("შეიყვანეთ თქვენი გვარი")
+ასაკი=input("შეიყვანეთ თქვენი ასაკი")
+სიმაღლე=input("შეიყვანეთ თქვენი სიმაღლე")
+
+
+
